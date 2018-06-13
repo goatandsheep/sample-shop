5a2bf5bbfad359bb263afe645fa10f31fc5e7998
Kemal Ahmed <goatandsheep@gmail.com>
2018-06-13 16:42:39 -0400

    changed project

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # sample-shop
 
-Test for GroupBy
+Test for CoinSmart
 
-## Assumptions made
+## API 
 
-* Latest Chrome browser
+Both endpoints should take only one parameter: an array of numbers called `unsortedNumbers`
 
-## Suggestions
+### Edge cases
 
-* I'd avoid using the Grid object altogether as to render the navs and products separately. Especially since the navs and products are loaded separately, I'd also want to use the data asynchronously separately.
-* I'd rename the HTML nav class to navs to be consistent with the javascript arrays
+Many edge cases were eliminated before I ran the array through the sorting algorithm by scanning for incorrect inputs. Inputs had to be an array of all numbers with no null, empty or undefined values, including empty arrays. I even handled badly formed json input. I also had to make sure I didn't remove `0`. I parsed all numbers as floats so any numbers with precision that couldn't be handled get rounded up. This was picked up by testing the type of input and the array length, as well as checking each value in the array for arrays or non-numbers (strings representing numbers are still cool). At the beginning I checked to see if `BodyParser` threw a syntax error. I also timed out after 10 seconds.
+
+### POST /bubbleSort
+
+> This endpoint will take in an array of numbers and rearrange them using the bubble sort algorithm 
+
+#### Time Complexity
+
+I'm sure you have all the time and space complexities memorized
+
+* **Worst**: O(n^2)
+* **Average**: Θ(n^2)
+* **Best**: Ω(n)
+
+
+
+### POST /mergeSort
+
+> This endpoint will take in an array of numbers and rearrange them using the merge sort algorithm
---
+++ index.js
@@ -0,0 +1,157 @@
+"use strict";
+
+const express = require('express')
+const app = express()
+const timeout = require('connect-timeout')
+
+const bodyParser = require('body-parser');
+
+app.use(timeout(10000));
+app.use(haltOnTimedout);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json());
+
+app.use(function(err, req, res, next) {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    res.status(400).json({
+      success: false,
+      message: 'Badly formed JSON'
+    });
+  }
+});
+
+function haltOnTimedout (req, res, next) {
+  res.status(408).json({
+    success: false,
+    message: 'Request timed out'
+  })
+}
+
+const cleanArray = (array, res) => {
+  const len = array.length;
+
+  if ( !Array.isArray(array) || !array) {
+    res.status(400).json({
+      success: false,
+      message: 'No array sent'
+    });
+    return;
+  }
+  else if (len === 0) {
+    res.send([]);
+    return;
+  }
+
+  for (let i = 0; i < len; i++) {
+    if (isNaN(array[i]) || !array[i] && array[i] != 0 || typeof array[i] === 'object') {
+      res.status(400).json({
+        success: false,
+        message: 'Non-number found in array'
+      });
+      return;
+    }
+    array[i] = parseFloat(array[i])
+    
+    if (array[i] > 1.7976931348623157e+308 || array[i] < -1.7976931348623157e+308) {
+      res.status(400).json({
+        success: false,
+        message: 'Array contains boundary number'
+      })
+      return;
+    }
+  }
+  return array;
+}
+
+// respond with "hello world" when a GET request is made to the homepage
+app.post('/mergeSort', (req, res) => {
+  let { unsortedNumbers } = req.body;
+
+  unsortedNumbers = cleanArray(unsortedNumbers, res);
+
+  const len = unsortedNumbers.length;
+
+  const mergeSort = (valuesA, valuesB) => {
+    if (valuesA.length > 1) {
+      valuesA = mergeSort(valuesA.slice(0,valuesA.length/2), valuesA.slice(valuesA.length/2));
+    }
+    if (valuesB.length > 1) {
+      valuesB = mergeSort(valuesB.slice(0,valuesB.length/2), valuesB.slice(valuesB.length/2));
+    }
+    let newValues = [];
+
+    // so we don't lose the shifted values
+    let currA = null;
+    let currB = null;
+
+    while (currA || currB || valuesA.length > 0 || valuesB.length > 0) {
+      // init
+      if (currA === null) {
+        currA = valuesA.shift();
+      }
+      if (currB === null) {
+        currB = valuesB.shift();
+      }
+
+      if (typeof currA === 'undefined') {
+        // valuesA is empty but not valuesB
+        newValues.push(currB);
+        currB = null; // I can shift here, but then valuesB may empty, preventing re-loop
+      } else if (typeof currB === 'undefined') {
+        // valuesB is empty but not valuesA
+        newValues.push(currA);
+        currA = null;
+      } else {
+        // both arrays contain values
+        if (currA < currB) {
+          newValues.push(currA);
+          currA = null;
+        } else {
+          newValues.push(currB);
+          currB = null;
+        }
+      }
+    }
+    return newValues;
+  }
+  let numbers = mergeSort(unsortedNumbers.slice(0,len/2),unsortedNumbers.slice(len/2));
+
+  res.status(200).json({
+    success: true,
+    data: numbers
+  });
+  return;
+})
+
+app.post('/bubbleSort', (req, res) => {
+  let { unsortedNumbers } = req.body
+
+  unsortedNumbers = cleanArray(unsortedNumbers, res)
+
+  let unsorted = true;
+
+  while (unsorted) {
+    let sorted = true;  // i.e. another pass needed
+    for (let i = 0; i < unsortedNumbers.length - 1; i++) {
+      if (unsortedNumbers[i] > unsortedNumbers[i+1]) {
+        let temp = unsortedNumbers[i];
+        unsortedNumbers[i] = unsortedNumbers[i+1];
+        unsortedNumbers[i+1] = temp;
+        sorted = false;
+      }
+    }
+    if (sorted) {
+      unsorted = false;
+    }
+  }
+  res.status(200).json({
+    success: true,
+    data: unsortedNumbers
+  });
+  return;
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
